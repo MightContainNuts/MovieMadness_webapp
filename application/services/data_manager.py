9a0c6bde9dc451d97857bbadbee942639200e809
MightContainNuts <mightcontainnuts@icloud.com>
2025-01-28 09:14:06 +0100

    remove .env files

--- application/services/data_manager.py
+++ application/services/data_manager.py
@@ -3,10 +3,31 @@ from abc import ABC, abstractmethod
 
 
 class DataManagerInterface(ABC):
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "Data Manager Interface"
+
+    def __repr__(self):
+        return "Data Manager Interface"
+
     @abstractmethod
     def get_all_users(self):
         pass
 
     @abstractmethod
     def get_all_movies(self):
+        pass
+
+    @abstractmethod
+    def add_user(self):
+        pass
+
+    @abstractmethod
+    def update_movie(self):
+        pass
+
+    @abstractmethod
+    def delete_movie(self):
         pass
--- application/services/sqllite_data_manager.py
+++ application/services/sqllite_data_manager.py
@@ -1,13 +1,21 @@
 # Description: all services related to the sql lite operations
-from application.app_factory import db
+# from application.app_factory import db
 from typing import override
+from logger import setup_logger
 
 from data_manager import DataManagerInterface
 
 
 class SQLiteDataManger(DataManagerInterface):
-    def __init__(self, db_file_name):
-        self.db = db(db_file_name)
+    def __init__(self):
+        self.logger = setup_logger(__name__)
+        self.logger.info("SQLite Data Manager created")
+
+    def __str__(self):
+        return "SQLite Data Manager"
+
+    def __repr__(self):
+        return "SQLite Data Manager"
 
     @override
     def get_all_users(self):
@@ -15,4 +23,16 @@ class SQLiteDataManger(DataManagerInterface):
 
     @override
     def get_all_movies(self):
+        pass
+
+    @override
+    def add_user(self):
+        pass
+
+    @override
+    def update_movie(self):
+        pass
+
+    @override
+    def delete_movie(self):
         pass
